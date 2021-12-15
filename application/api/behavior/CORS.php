12b1fabf88118d175427353e20a42a5b17ee32ed
feiy <648039341@qq.com>
2021-12-15 19:22:31 +0800

    'yan'

---
+++ application/api/behavior/CORS.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace app\api\behavior;
+
+
+class CORS
+{
+    public function appInit()
+    {
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Headers: Authorization,Origin, X-Requested-With, Content-Type, Accept");
+        header('Access-Control-Expose-Headers: Authorization');
+        header('Access-Control-Allow-Methods: PUT,DELETE,GET,POST');
+        if(request()->isOptions()){
+            exit();
+        }
+    }
+}
