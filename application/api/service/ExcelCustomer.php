8ebe6da98b94a0a3b576e5cd2a44c1281fdffac1
feiy <648039341@qq.com>
2022-01-13 22:11:46 +0800

    'yan'

--- application/api/service/ExcelCustomer.php
+++ application/api/service/ExcelCustomer.php
@@ -192,10 +192,11 @@ class ExcelCustomer
 
 
         // 建立excel对象
-        var_dump($file);
+        var_dump($objRead);
+        return false;
         $obj = $objRead->load($file);
         var_dump($obj);
-        return false;
+
 
 
         // 获取指定的sheet表
