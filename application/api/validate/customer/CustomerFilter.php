694d0958ad110445cd1e597db32aa7dddcbe1307
feiy <648039341@qq.com>
2022-03-18 16:27:05 +0800

    'yan'

--- application/api/validate/customer/CustomerFilter.php
+++ application/api/validate/customer/CustomerFilter.php
@@ -12,6 +12,7 @@ class CustomerFilter extends BaseValidate
         'start|开始时间' => 'date',
         'end|结束时间' => 'date',
         'name|客户名' => 'chs',
-        'user_code|客户编码' => 'alphaNum|length:8'
+        'user_code|客户编码' => 'alphaNum'
     ];
+//|length:8
 }
