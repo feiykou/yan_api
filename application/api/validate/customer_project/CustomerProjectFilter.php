04787abf3f65a5cfdff5aaa44e3a2f938b1a898c
feiy <648039341@qq.com>
2022-03-18 16:21:56 +0800

    'yan'

--- application/api/validate/customer_project/CustomerProjectFilter.php
+++ application/api/validate/customer_project/CustomerProjectFilter.php
@@ -11,6 +11,6 @@ class CustomerProjectFilter extends BaseValidate
     protected $rule = [
         'start|开始时间' => 'date',
         'end|结束时间' => 'date',
-        'name|客户名' => 'chs'
+//        'name|客户名' => 'chs'
     ];
 }
